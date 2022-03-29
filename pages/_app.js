0c9d90eeba399add92739d5b6eef26aea84979b8
riandyhasan <riandyhsn@gmail.com>
2022-03-30 05:35:51 +0700

    init

---
+++ pages/_app.js
@@ -0,0 +1,36 @@
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import { Global } from "@emotion/react";
+import "../styles/globals.css";
+
+const theme = extendTheme({
+  fonts: {
+    heading: "Montserrat, sans-serif",
+    body: "Montserrat, sans-serif",
+  },
+  colors: {
+    primary: {
+      green: "#077F46",
+      orange: "#F18720",
+      gray: "#767575",
+      black: "#353535"
+    },
+    secondary: {
+      green: "#33665D",
+      orange: "#EE873B",
+      yellow: "#EFC43B",
+      gray: "#C5C5C5",
+      black: "#353535"
+    },
+  },
+});
+
+function App({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Global />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
+}
+
+export default App;
